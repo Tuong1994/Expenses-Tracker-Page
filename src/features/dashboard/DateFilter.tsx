3eb2f9b5b3f3e63d15186622d2c9397a8650c81f
Tuong1994 <nhambontuong68@gmail.com>
2025-12-03 16:57:02 +0700

    change meta message

--- src/features/dashboard/DateFilter.tsx
+++ src/features/dashboard/DateFilter.tsx
@@ -5,6 +5,7 @@ import { Flex } from "@/components/UI";
 import { DatePicker } from "@/components/Control";
 import { ControlColor } from "@/components/Control/type";
 import useLayout from "@/components/UI/Layout/useLayout";
+import moment from "moment";
 
 const { FlexRow, FlexCol } = Flex;
 
@@ -18,10 +19,18 @@ const DateFilter: FC<DateFilterProps> = ({ className }) => {
   return (
     <FlexRow rootClassName={className} justify="between">
       <FlexCol xs={24} md={12} lg={12} span={12}>
-        <DatePicker placement="right" color={layoutValue.layoutColor as ControlColor} />
+        <DatePicker
+          placement="right"
+          color={layoutValue.layoutColor as ControlColor}
+          onChangeSelect={(date) => console.log("start", moment(date).format('YYYY-MM-DD'))}
+        />
       </FlexCol>
       <FlexCol xs={24} md={12} lg={12} span={12}>
-        <DatePicker placement="right" color={layoutValue.layoutColor as ControlColor} />
+        <DatePicker
+          placement="right"
+          color={layoutValue.layoutColor as ControlColor}
+          onChangeSelect={(date) => console.log("end", date.toISOString())}
+        />
       </FlexCol>
     </FlexRow>
   );
